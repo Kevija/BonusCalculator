a21be5c3834df5562eed4ed324b053575ee27415
ek <evija.kvante@inbox.lv>
2022-03-07 20:52:43 +0200

    Initial commit

---
+++ src/main/scala/BonusCalculator.scala
@@ -0,0 +1,23 @@
+import scala.io.StdIn.readLine
+
+  object BonusCalculator extends App {
+
+    println("Christmas bonus calculator")
+
+    println("Please enter your name:")
+    val personsName = readLine
+    println(s"Hello, $personsName! Please enter how many years you are working at this firm:")
+    val workingYears = readLine().toInt
+    println(s"Great, $personsName! Please enter your monthly wage:")
+    val monthlyWage = readLine().toDouble
+
+    if (workingYears > 2 || workingYears==2) {
+      val bonusMoney = (monthlyWage * 15 / 100) * workingYears
+      println(s"Your monthlyWage: $monthlyWage, your Christmas bonus: $bonusMoney")
+    }
+    else {
+      println(s"$personsName, sorry this year you have no bonuses :(")
+    }
+  }
+
+
